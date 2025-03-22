4894a522c1d8e839a41f67bb4a119dfeaf4922d6
kenebebh <kenebebhbanigo@gmail.com>
2025-03-23 00:51:35 +0100

    light and dark mode theme provider and toggle

---
+++ src/components/ui/layout/navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ThemeToggle } from "@/helpers/ThemeToggle";
+
+export default function Navbar() {
+  return (
+    <div>
+      <ThemeToggle />
+    </div>
+  );
+}
